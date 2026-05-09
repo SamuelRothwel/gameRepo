f46bf268f23b2511387610945652d187f3302c37
Samuel Rothwell <Samuel.Rothwell@wisetechglobal.com>
2026-05-09 17:37:08 +1000

    sprite order

--- scripts/UIScripts/SpriteScrollContainer.cs
+++ scripts/UIScripts/SpriteScrollContainer.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 
 public partial class SpriteScrollContainer : VBoxContainer
 {
+	const float DragThreshold = 6f;
 	public Vector2 spriteSize;
 	public Dictionary<string, Sprite2D> sprites;
+	public Dictionary<string, Control> spriteSlots;
 	public Vector2 marginLength;
 	public int borderLength;
+	string draggedSpriteName;
+	Control draggedSlot;
+	Vector2 dragStartPosition;
+	bool dragMoved;
 	public override void _Ready()
 	{
 		((ScrollContainer)GetParent()).MouseForcePassScrollEvents = false;
 		sprites = new Dictionary<string, Sprite2D>();
+		spriteSlots = new Dictionary<string, Control>();
 		spriteSize = math.createVector(180);
 		marginLength = math.createVector(10);
 		borderLength = 5;
@@ -23,9 +30,10 @@ public partial class SpriteScrollContainer : VBoxContainer
 		{
 			sprites[e.name].Texture = e.sprite;
 			setSprite(sprites[e.name]);
+			setSlotOrder(e.name, e.order);
 		} else
 		{
-			addSprite(e.name, e.sprite);
+			addSprite(e.name, e.sprite, e.order);
 		}
 	}
 	void setSprite(Sprite2D sprite)
@@ -34,20 +42,111 @@ public partial class SpriteScrollContainer : VBoxContainer
 		float textureScale = Math.Min(spriteSize.X/textureSize.X,spriteSize.Y/textureSize.Y);
 		sprite.Scale = math.createVector(textureScale);
 	}
-	void addSprite(string Name, Texture2D texture)
+	void addSprite(string Name, Texture2D texture, int order)
 	{
-		Control slot = new Control();
-		slot.CustomMinimumSize = spriteSize;
+		Button slot = new Button();
+		slot.CustomMinimumSize = spriteSize + marginLength * 2;
+		slot.Text = "";
+		slot.Flat = true;
+		slot.GuiInput += inputEvent => OnSlotGuiInput(Name, slot, inputEvent);
 		AddChild(slot);
 
 		Sprite2D sprite = new Sprite2D();
 		sprite.Texture = texture;
 		sprite.Centered = true;
-		sprite.Position = (spriteSize+marginLength*2)/2;
+		sprite.Position = slot.CustomMinimumSize / 2;
 		setSprite(sprite);
 		slot.AddChild(sprite);
 		mAccess.layerManager.addLayer(sprite, "UI2");
 		sprites[Name] = sprite;
+		spriteSlots[Name] = slot;
+		setSlotOrder(Name, order);
+	}
+	void setSlotOrder(string Name, int order)
+	{
+		if (!spriteSlots.ContainsKey(Name))
+		{
+			return;
+		}
+
+		if (order < 0)
+		{
+			order = mAccess.creatorManager.GetSpriteLayerOrder(Name);
+		}
+
+		if (order >= 0)
+		{
+			MoveChild(spriteSlots[Name], order);
+		}
+	}
+	void OnSlotGuiInput(string Name, Control slot, InputEvent inputEvent)
+	{
+		if (inputEvent is InputEventMouseButton mouseButton && mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
+		{
+			draggedSpriteName = Name;
+			draggedSlot = slot;
+			dragStartPosition = mouseButton.GlobalPosition;
+			dragMoved = false;
+			mAccess.creatorManager.SetActiveSpriteLayer(Name);
+		}
+	}
+	public override void _Input(InputEvent inputEvent)
+	{
+		if (draggedSlot == null)
+		{
+			return;
+		}
+
+		if (inputEvent is InputEventMouseButton mouseButton && mouseButton.ButtonIndex == MouseButton.Left && !mouseButton.Pressed)
+		{
+			FinishDrag();
+		}
+		else if (inputEvent is InputEventMouseMotion mouseMotion)
+		{
+			UpdateDrag(mouseMotion.GlobalPosition);
+		}
+	}
+	void UpdateDrag(Vector2 globalPosition)
+	{
+		if (!dragMoved && dragStartPosition.DistanceTo(globalPosition) < DragThreshold)
+		{
+			return;
+		}
+
+		dragMoved = true;
+		int order = GetSlotOrderFromGlobalY(globalPosition.Y);
+		if (order >= 0 && order != draggedSlot.GetIndex())
+		{
+			MoveChild(draggedSlot, order);
+		}
+	}
+	void FinishDrag()
+	{
+		if (dragMoved)
+		{
+			mAccess.creatorManager.MoveSpriteLayer(draggedSpriteName, draggedSlot.GetIndex());
+		}
+		else
+		{
+			mAccess.creatorManager.SetActiveSpriteLayer(draggedSpriteName);
+		}
+
+		draggedSpriteName = null;
+		draggedSlot = null;
+		dragMoved = false;
+	}
+	int GetSlotOrderFromGlobalY(float globalY)
+	{
+		for (int i = 0; i < GetChildCount(); i++)
+		{
+			Control slot = GetChild<Control>(i);
+			if (globalY < slot.GlobalPosition.Y + slot.Size.Y / 2f)
+			{
+				return i;
+			}
+		}
+
+		return GetChildCount() - 1;
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
--- scripts/managerScripts/creatorManagement.cs
+++ scripts/managerScripts/creatorManagement.cs
@@ -4,20 +4,28 @@ using System.Collections.Generic;
 
 public partial class CreatorManagement : managerNode
 {
+	const float InactiveLayerAlphaMultiplier = 0.5f;
 	public Sprite2D tempSprite;
 	public Sprite2D activeSprite;
 	public Vector2 activeSpriteCoords;
 	public float spriteScale;
 	public event EventHandler<SpriteEvent> spriteChangeEvent;
 	public Dictionary<string, (Sprite2D, List<Vector2>)> activeSpriteLayers;
+	public Dictionary<string, Vector2> activeSpriteLayerCoords;
+	public Dictionary<string, Color> activeSpriteLayerColors;
+	public List<string> activeSpriteLayerOrder;
 	string activeLayer;
 	public Dictionary<Godot.Key, string> keyBinds;
 	public string activeFunction;
 	public override void setup()
 	{
 		activeSpriteLayers = new Dictionary<string, (Sprite2D, List<Vector2>)>();
+		activeSpriteLayerCoords = new Dictionary<string, Vector2>();
+		activeSpriteLayerColors = new Dictionary<string, Color>();
+		activeSpriteLayerOrder = new List<string>();
 		keyBinds = new Dictionary<Key, string>();
 		activeFunction = "draw";
+		mAccess.colorManager.colorChanged += OnColorChanged;
 	}
 	public void AddSpriteLayer(string name = "", int scale = 10)
 	{
@@ -26,19 +34,140 @@ public partial class CreatorManagement : managerNode
 			name = "Sprite layer " + activeSpriteLayers.Count;
 		}
 		
-		activeSprite.TryQueueFree();
 		activeSprite = new Sprite2D();
 		activeSprite.Scale = new Vector2(scale, scale);
 		AddChild(activeSprite);
 		activeSprite.Texture = ImageTexture.CreateFromImage(Image.Create(1, 1, false, Image.Format.Rgba8));
 		activeSpriteLayers[name] = (activeSprite, new List<Vector2>());
-		activeLayer = name;
+		activeSpriteLayerCoords[name] = math.createVector(0);
+		activeSpriteLayerColors[name] = mAccess.colorManager.getActiveColor();
+		activeSpriteLayerOrder.Add(name);
+		SetActiveSpriteLayer(name);
 		spriteScale = scale;
 		activeSprite.Centered = false;
-		activeSpriteCoords = math.createVector(0);
+	}
+	public void SetActiveSpriteLayer(string name)
+	{
+		if (!activeSpriteLayers.ContainsKey(name))
+		{
+			return;
+		}
+
+		string oldActiveLayer = activeLayer;
+		activeLayer = name;
+		activeSprite = activeSpriteLayers[name].Item1;
+		activeSpriteCoords = activeSpriteLayerCoords[name];
+		spriteScale = activeSprite.Scale.X;
+		UpdateSpriteLayerZIndexes();
+
+		if (oldActiveLayer != null && oldActiveLayer != activeLayer)
+		{
+			RedrawSpriteLayer(oldActiveLayer, false);
+		}
+
+		RedrawSpriteLayer(activeLayer, true);
+	}
+	public int GetSpriteLayerOrder(string name)
+	{
+		return activeSpriteLayerOrder.IndexOf(name);
+	}
+	public void MoveSpriteLayer(string name, int order)
+	{
+		int oldOrder = GetSpriteLayerOrder(name);
+		if (oldOrder < 0)
+		{
+			return;
+		}
+
+		order = Math.Clamp(order, 0, activeSpriteLayerOrder.Count - 1);
+		if (oldOrder == order)
+		{
+			return;
+		}
+
+		activeSpriteLayerOrder.RemoveAt(oldOrder);
+		activeSpriteLayerOrder.Insert(order, name);
+		UpdateSpriteLayerZIndexes();
+	}
+	void UpdateSpriteLayerZIndexes()
+	{
+		int activeOrder = GetSpriteLayerOrder(activeLayer);
+		for (int i = 0; i < activeSpriteLayerOrder.Count; i++)
+		{
+			string name = activeSpriteLayerOrder[i];
+			activeSpriteLayers[name].Item1.ZIndex = i - activeOrder;
+		}
+	}
+	void RedrawSpriteLayer(string name, bool selected)
+	{
+		if (!activeSpriteLayers.ContainsKey(name))
+		{
+			return;
+		}
+
+		Image image;
+		List<Vector2> coords = activeSpriteLayers[name].Item2;
+		if (coords.Count == 0)
+		{
+			image = Image.Create(1, 1, false, Image.Format.Rgba8);
+		}
+		else
+		{
+			image = mAccess.spriteManager.vectorsToImage(coords, "black");
+			ApplySavedColor(image, activeSpriteLayerColors[name]);
+		}
+
+		if (!selected)
+		{
+			HalveImageAlpha(image);
+		}
+
+		activeSpriteLayers[name].Item1.Texture = ImageTexture.CreateFromImage(image);
+		spriteChangeEvent?.Invoke(this, new SpriteEvent(activeSpriteLayers[name].Item1.Texture, name, GetSpriteLayerOrder(name)));
+	}
+	void HalveImageAlpha(Image image)
+	{
+		for (int x = 0; x < image.GetWidth(); x++)
+		{
+			for (int y = 0; y < image.GetHeight(); y++)
+			{
+				Color color = image.GetPixel(x, y);
+				color.A *= InactiveLayerAlphaMultiplier;
+				image.SetPixel(x, y, color);
+			}
+		}
+	}
+	void ApplySavedColor(Image image, Color savedColor)
+	{
+		for (int x = 0; x < image.GetWidth(); x++)
+		{
+			for (int y = 0; y < image.GetHeight(); y++)
+			{
+				Color color = image.GetPixel(x, y);
+				if (color.A > 0)
+				{
+					Color recolored = savedColor;
+					recolored.A *= color.A;
+					image.SetPixel(x, y, recolored);
+				}
+			}
+		}
+	}
+	void OnColorChanged(object sender, ColorChangedEvent e)
+	{
+		if (e.name == mAccess.colorManager.activeColorName && activeLayer != null)
+		{
+			activeSpriteLayerColors[activeLayer] = e.color;
+			RedrawSpriteLayer(activeLayer, true);
+		}
 	}
 	public void hover(Vector2 coord)
 	{
+		if (activeSprite == null || activeLayer == null)
+		{
+			return;
+		}
+
 		switch (activeFunction)
 		{
 			case "draw":
@@ -53,12 +182,18 @@ public partial class CreatorManagement : managerNode
 						activeSpriteLayers[activeLayer].Item2[i] -= negDif;
 					}
 					activeSprite.Position = activeSpriteCoords * spriteScale;
+					activeSpriteLayerCoords[activeLayer] = activeSpriteCoords;
 				}
 				break;
 		}
 	}
 	public void click(Vector2 coord)
 	{
+		if (activeSprite == null || activeLayer == null)
+		{
+			return;
+		}
+
 		switch (activeFunction)
 		{
 			case "draw":
@@ -73,10 +208,10 @@ public partial class CreatorManagement : managerNode
 						activeSpriteLayers[activeLayer].Item2[i] -= negDif;
 					}
 					activeSprite.Position = activeSpriteCoords * spriteScale;
+					activeSpriteLayerCoords[activeLayer] = activeSpriteCoords;
 				}
 				activeSpriteLayers[activeLayer].Item2.Add(dif);
-				activeSprite.Texture = ImageTexture.CreateFromImage(mAccess.spriteManager.vectorsToImage(activeSpriteLayers[activeLayer].Item2, mAccess.colorManager.activeColorName));
-				spriteChangeEvent?.Invoke(this, new SpriteEvent(activeSprite.Texture, activeLayer));
+				RedrawSpriteLayer(activeLayer, true);
 				break;
 		}
 	}
@@ -93,9 +228,11 @@ public class SpriteEvent : EventArgs
 {
 	public Texture2D sprite {get;}
 	public string name{get;}
-	public SpriteEvent(Texture2D spriteArg, string nameArg)
+	public int order {get;}
+	public SpriteEvent(Texture2D spriteArg, string nameArg, int orderArg = -1)
 	{
 		sprite = spriteArg;
 		name = nameArg;
+		order = orderArg;
 	}
 }
